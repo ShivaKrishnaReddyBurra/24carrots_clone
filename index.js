630a92a3667efdbc81bc655276c331b6d4e0cd6c
BURRA SHIVA KRISHNA REDDY <123821882+ShivaKrishnaReddyBurra@users.noreply.github.com>
2024-02-17 02:49:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function side(){
+    var sidebar=document.getElementById("a");
+    sidebar.style.display="flex";
+}
+function sclose(){
+    var sidebar=document.getElementById("a");
+    sidebar.style.display="none";
+}
